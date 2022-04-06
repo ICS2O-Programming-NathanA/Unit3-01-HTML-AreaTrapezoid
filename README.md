8c3533c963803034ee1e1c25556be395843ec481
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-04-06 20:18:45 +0000

    added js, css and html

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Unit3-01-HTML-AreaTrapezoid
+[![GitHub's Super Linter](https://github.com/ICS2O-Programming-NathanA/Unit3-01-HTML-AreaTrapezoid/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/ICS2O-Programming-NathanA/Unit3-01-HTML-AreaTrapezoid/actions)
---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Nathan Araujo All rights reserved
+//
+// Created by: Nathan Araujo
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calcAreaClicked () {
+  // get user input
+  let side_a = parseFloat(document.getElementById('side-a').value)
+  let side_b = parseFloat(document.getElementById('side-b').value)
+	let height = parseFloat(document.getElementById('height').value)
+
+
+  // calculate the area
+  let area = (side_a + side_b) / 2 * height 
+
+  // display the results
+  document.getElementById('area').innerHTML = "The area is " + area.toFixed(2) + "cm<sup>2</sup>"
+}
